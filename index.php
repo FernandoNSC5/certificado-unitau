7e29068f92871e4a38aafa4bcd3ee0a4dd090318
fernandonsc5 <fernandonsc5@gmail.com>
2018-10-31 13:48:12 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!-------------------------------------
+   @Author: Fernando N. S. Costa
+   Universidade de Taubaté - 2018
+   https://www.linkedin.com/in/fernandonsc5/
+--------------------------------------->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		
+		<title>Unitau - Certificados</title>
+		
+		<!-- Basics -->
+		<meta charset="UTF-8">
+		<meta name="viewport" contente="width=device-width, initial-scale=1">
+
+		<!-- Main css & w3School framework -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+	</head>
+
+	<body>
+
+		<div class="background w3-animate-opacity" style="background-image: url('images/bg-01.jpg');">
+			<div class="container">
+				<div class="wrap w3-animate-opacity">
+
+					<form class="certificate-form validate-form">
+						<!-- Top form title -->
+						<span class="form-title">
+							Certificados
+							<p>Preencha um dos campos abaixo</p>
+						</span>
+
+						<!-- form data -->
+						<div class="wrap-input">
+							<input id="cpf" class="input" type="text" name="cpf" placeholder="CPF">
+							<span class="focus-input"/>
+						</div>
+
+						<div class="wrap-input">
+							<input id="name" class="input" tyle="text" name="name" placeholder="Nome Completo">
+							<span class="focus-input"/>
+						</div>
+
+						<!-- submit form button -->
+						<div class="container-form-btn" >
+							<button class="form-btn">
+								Gerar Certificado
+							</button>
+						</div>
+					</form>
+
+				</div>
+				<p class="footer">Universidade de Taubaté<br>2018</p>
+			</div>
+		</div>
+
+	</body>
+
+</html>
